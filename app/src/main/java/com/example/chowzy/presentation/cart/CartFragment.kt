56b2b915c441cf5eedd25bd889f2aa24b6f66a86
Daud Dhiya <daud.dhiya31@gmail.com>
2024-03-29 23:49:30 +0700

    feat: cart page

--- app/src/main/java/com/example/chowzy/presentation/cart/CartFragment.kt
+++ app/src/main/java/com/example/chowzy/presentation/cart/CartFragment.kt
@@ -1,20 +1,127 @@
 package com.example.chowzy.presentation.cart
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.chowzy.R
+import com.example.chowzy.data.datasource.cart.CartDataSource
+import com.example.chowzy.data.datasource.cart.CartDatabaseDataSource
+import com.example.chowzy.data.model.Cart
+import com.example.chowzy.data.repository.cart.CartRepository
+import com.example.chowzy.data.repository.cart.CartRepositoryImpl
+import com.example.chowzy.data.source.local.database.AppDatabase
 import com.example.chowzy.databinding.FragmentCartBinding
+import com.example.chowzy.presentation.checkout.CheckoutActivity
+import com.example.chowzy.presentation.common.adapter.CartListAdapter
+import com.example.chowzy.presentation.common.adapter.CartListener
+import com.example.chowzy.utils.GenericViewModelFactory
+import com.example.chowzy.utils.hideKeyboard
+import com.example.chowzy.utils.proceedWhen
+import com.example.chowzy.utils.toRupiahFormat
 
 class CartFragment : Fragment() {
+
     private lateinit var binding: FragmentCartBinding
+    private val viewModel: CartViewModel by viewModels {
+        val db = AppDatabase.getInstance(requireContext())
+        val ds: CartDataSource = CartDatabaseDataSource(db.cartDao())
+        val rp: CartRepository = CartRepositoryImpl(ds)
+        GenericViewModelFactory.create(CartViewModel(rp))
+    }
+    private val adapter: CartListAdapter by lazy {
+        CartListAdapter(object : CartListener {
+            override fun onPlusTotalItemCartClicked(cart: Cart) {
+                viewModel.increaseCart(cart)
+            }
+
+            override fun onMinusTotalItemCartClicked(cart: Cart) {
+                viewModel.decreaseCart(cart)
+            }
+
+            override fun onRemoveCartClicked(cart: Cart) {
+                viewModel.removeCart(cart)
+            }
+
+            override fun onUserDoneEditingNotes(cart: Cart) {
+                viewModel.setCartNotes(cart)
+                hideKeyboard()
+            }
+
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Inflate the layout for this fragment
         binding = FragmentCartBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupList()
+        observeData()
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        binding.btnCheckout.setOnClickListener {
+            startActivity(Intent(requireContext(), CheckoutActivity::class.java))
+        }
+    }
+
+    private fun observeData() {
+        viewModel.getAllCarts().observe(viewLifecycleOwner) { result ->
+            result.proceedWhen(
+                doOnLoading = {
+                    binding.layoutState.root.isVisible = true
+                    binding.layoutState.pbLoading.isVisible = true
+                    binding.layoutState.tvError.isVisible = false
+                    binding.rvCart.isVisible = false
+                },
+                doOnSuccess = {
+                    binding.layoutState.root.isVisible = false
+                    binding.layoutState.pbLoading.isVisible = false
+                    binding.layoutState.tvError.isVisible = false
+                    binding.rvCart.isVisible = true
+                    result.payload?.let { (carts, totalPrice) ->
+                        // set list cart data
+                        adapter.submitData(carts)
+                        binding.tvTotalPrice.text = totalPrice.toRupiahFormat()
+
+                    }
+                },
+                doOnError = {
+                    binding.layoutState.root.isVisible = true
+                    binding.layoutState.pbLoading.isVisible = false
+                    binding.layoutState.tvError.isVisible = true
+                    binding.layoutState.tvError.text = result.exception?.message.orEmpty()
+                    binding.rvCart.isVisible = false
+                },
+                doOnEmpty = {
+                    binding.layoutState.root.isVisible = true
+                    binding.layoutState.pbLoading.isVisible = false
+                    binding.layoutState.tvError.isVisible = true
+                    binding.layoutState.tvError.text = getString(R.string.text_cart_is_empty)
+                    binding.rvCart.isVisible = false
+                    result.payload?.let { (carts, totalPrice) ->
+                        binding.tvTotalPrice.text = totalPrice.toRupiahFormat()
+                    }
+                }
+            )
+
+        }
+    }
+
+    private fun setupList() {
+        binding.rvCart.adapter = this@CartFragment.adapter
+    }
+
 }
--- app/src/main/java/com/example/chowzy/presentation/cart/CartViewModel.kt
+++ app/src/main/java/com/example/chowzy/presentation/cart/CartViewModel.kt
@@ -1,7 +1,35 @@
 package com.example.chowzy.presentation.cart
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.chowzy.data.model.Cart
+import com.example.chowzy.data.repository.cart.CartRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class CartViewModel : ViewModel() {
-
+class CartViewModel(private val cartRepository: CartRepository): ViewModel() {
+    fun getAllCarts() = cartRepository.getUserCartData().asLiveData(Dispatchers.IO)
+    val cartList = cartRepository.getUserCartData().asLiveData(Dispatchers.IO)
+    fun decreaseCart(item: Cart) {
+        viewModelScope.launch(Dispatchers.IO) {
+            cartRepository.decreaseCart(item).collect()
+        }
+    }
+    fun increaseCart(item: Cart) {
+        viewModelScope.launch(Dispatchers.IO) {
+            cartRepository.increaseCart(item).collect()
+        }
+    }
+    fun removeCart(item: Cart) {
+        viewModelScope.launch(Dispatchers.IO) {
+            cartRepository.deleteCart(item).collect()
+        }
+    }
+    fun setCartNotes(item: Cart) {
+        viewModelScope.launch(Dispatchers.IO) {
+            cartRepository.setCartNotes(item).collect()
+        }
+    }
 }
