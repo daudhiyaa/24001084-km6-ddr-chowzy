d948ff1f334658e6df535726d5e6fbdb26568e53
Daud Dhiya <daud.dhiya31@gmail.com>
2024-03-29 23:44:09 +0700

    feat: finishing profile page

--- app/src/main/java/com/example/chowzy/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/chowzy/presentation/profile/ProfileFragment.kt
@@ -5,16 +5,57 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.example.chowzy.R
 import com.example.chowzy.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
 
+    private val viewModel: ProfileViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
         return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListener()
+        observeEditMode()
+        observeProfileData()
+    }
+
+    private fun observeProfileData() {
+        viewModel.profileData.observe(viewLifecycleOwner) {
+            binding.layoutProfileBody.ivProfilePicture.load(it.profileImg) {
+                crossfade(true)
+                error(R.drawable.ic_profile)
+                transformations(CircleCropTransformation())
+            }
+            binding.layoutProfileBody.etName.setText(it.name)
+            binding.layoutProfileBody.etUsername.setText(it.username)
+            binding.layoutProfileBody.etEmail.setText(it.email)
+        }
+    }
+
+    private fun observeEditMode() {
+        viewModel.isEditMode.observe(viewLifecycleOwner) {
+            binding.layoutProfileBody.etUsername.isEnabled = it
+            binding.layoutProfileBody.etEmail.isEnabled = it
+        }
+    }
+
+    private fun setClickListener() {
+        binding.layoutProfileHeader.ivEditProfile.setOnClickListener {
+            viewModel.changeEditMode()
+        }
     }
 }
--- app/src/main/java/com/example/chowzy/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/chowzy/presentation/profile/ProfileViewModel.kt
@@ -1,6 +1,23 @@
 package com.example.chowzy.presentation.profile
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.chowzy.data.model.Profile
 
 class ProfileViewModel : ViewModel() {
+    val profileData = MutableLiveData(
+        Profile(
+            name = "Daud Dhiya'",
+            username = "daudhiyaa",
+            email = "daud.dhiya31@gmail.com",
+            profileImg = "https://avatars.githubusercontent.com/u/90663569?v=4"
+        )
+    )
+
+    val isEditMode = MutableLiveData(false)
+
+    fun changeEditMode() {
+        val currentValue = isEditMode.value ?: false
+        isEditMode.postValue(!currentValue)
+    }
 }
